c089b4fb69ccc166c15db2d2aec9fefd6af77d27
DekelDor94 <102503633+DekelDor94@users.noreply.github.com>
2023-06-24 10:07:32 +0300

    Add files via upload

--- request.c
+++ request.c
@@ -206,7 +206,7 @@ void requestHandle(int fd, threadNode* threadyNode)
    rio_t rio;
 
    Rio_readinitb(&rio, fd);
-   printf("fd: %d\n",fd);
+   //printf("fd: %d\n",fd);
    Rio_readlineb(&rio, buf, MAXLINE);
    sscanf(buf, "%s %s %s", method, uri, version);
 
